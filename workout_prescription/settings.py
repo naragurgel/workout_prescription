8d7c701bfd5f07a35f2af56c98e65734b84eb1dc
naragurgel <gurgel.nara@gmail.com>
2023-03-24 15:20:44 +0000

    set up bootstrap

--- workout_prescription/settings.py
+++ workout_prescription/settings.py
@@ -46,10 +46,16 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
-    'prescriptions'
+    'prescriptions',
+    'crispy_forms',
+    "crispy_bootstrap5",
 ]
 
 SITE_ID = 1
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
