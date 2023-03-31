0ff806430039362eaeb34e98a72e3257db74b975
naragurgel <gurgel.nara@gmail.com>
2023-03-31 17:04:13 +0000

    add home page template/ view

--- workout_prescription/urls.py
+++ workout_prescription/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from prescriptions.views import homepage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("accounts/", include("allauth.urls")),
+    path('', homepage.HomePageView.as_view(), name='homepage'),
     path("prescriptions/", include("prescriptions.urls")),
 ]
