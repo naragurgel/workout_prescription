f17f744bc85520fa4b419ba0806a69fec541c066
naragurgel <gurgel.nara@gmail.com>
2023-05-10 20:29:06 +0000

    changed database on settings

--- workout_prescription/settings.py
+++ workout_prescription/settings.py
@@ -92,11 +92,18 @@ WSGI_APPLICATION = 'workout_prescription.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+# Database
+# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 AUTH_PASSWORD_VALIDATORS = [
