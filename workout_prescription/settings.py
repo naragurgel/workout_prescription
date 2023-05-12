a36a9cd635a80f69b3827fa3afae1924a85cc6ca
naragurgel <gurgel.nara@gmail.com>
2023-05-12 11:16:54 +0000

    updated settings static

--- workout_prescription/settings.py
+++ workout_prescription/settings.py
@@ -147,7 +147,7 @@ CSRF_TRUSTED_ORIGINS = ['workout-prescriptions.herokuapp.com', '8000-naragurgel-
 
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
