d7c2c361fa99de558dad241a4902047a645e4c38
naragurgel <gurgel.nara@gmail.com>
2023-05-22 18:06:56 +0000

    changed nav bar

--- prescriptions/urls.py
+++ prescriptions/urls.py
@@ -11,8 +11,8 @@ urlpatterns = [
     path('workout/update/<int:pk>/', workout.update, name='workout_update'),
     path('workout/delete/<int:pk>/', workout.delete, name='workout_delete'),
     path('workout_item/', workout_item.list, name='workout_item_list'),
-    path('workout_item/create/', workout_item.create, name='workout_item_create'),
-    path('workout_item/update/<int:pk>/', workout_item.update, name='workout_item_update'),
-    path('workout_item/delete/<int:pk>/', workout_item.delete, name='workout_item_delete'),
+    path('workout_item/create/', workout_item.create, name='workout_item_create'),  # noqa
+    path('workout_item/update/<int:pk>/', workout_item.update, name='workout_item_update'),  # noqa
+    path('workout_item/delete/<int:pk>/', workout_item.delete, name='workout_item_delete'),  # noqa
 
 ]
