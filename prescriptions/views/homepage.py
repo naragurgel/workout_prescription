c5012f4b837992888ee081174352d5bbdf84f590
naragurgel <gurgel.nara@gmail.com>
2023-05-23 19:56:10 +0000

    setting up error pages 500/403/404

--- prescriptions/views/homepage.py
+++ prescriptions/views/homepage.py
@@ -3,3 +3,24 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name = "index.html"
+
+
+def error_404_view(request, exception):
+    """
+    Displays 404.html path
+    """
+
+    return render(request, '404.html')
+
+
+def handler500(request, *args, **argv):
+    """
+    Displays 500.html path
+    """
+    return render(request, '500.html')
+
+def handler403(request, *args, **argv):
+    """
+    Displays 403.html path
+    """
+    return render(request, '403.html')
