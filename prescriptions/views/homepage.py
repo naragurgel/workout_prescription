e79628f3088161a34fe42cc94add46de6406c2b8
naragurgel <gurgel.nara@gmail.com>
2023-05-24 11:29:34 +0000

    updated views

--- prescriptions/views/homepage.py
+++ prescriptions/views/homepage.py
@@ -3,14 +3,15 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import Context, loader
 
+
 class HomePageView(TemplateView):
     template_name = "index.html"
 
 
 def error404(request, exception):
-     template = loader.get_template('404.html')
-     context = Context({'message': 'All: %s' % request,})
-     return HttpResponse(content=template.render(context), content_type='text/html; charset=utf-8', status=404)
+    template = loader.get_template('404.html')
+    context = Context({'message': 'All: %s' % request, })
+    return HttpResponse(content=template.render(context), content_type='text/html; charset=utf-8', status=404)  # noqa
 
 
 def handler500(request, *args, **argv):
@@ -19,10 +20,9 @@ def handler500(request, *args, **argv):
     """
     return render(request, '500.html')
 
+
 def handler403(request, *args, **argv):
     """
     Displays 403.html path
     """
     return render(request, '403.html')
-
-    
